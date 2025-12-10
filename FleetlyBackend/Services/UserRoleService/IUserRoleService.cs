96a613ad728d0972a49502ed89a5da6dcc0d1a0f
MielnickiB <mielnicki.bartek@gmail.com>
2025-12-10 02:40:26 +0100

    Update dla UserRole

--- FleetlyBackend/Services/UserRoleService/IUserRoleService.cs
+++ FleetlyBackend/Services/UserRoleService/IUserRoleService.cs
@@ -4,7 +4,7 @@ namespace FleetlyBackend.Services.UserRoleService
 {
     public interface IUserRoleService
     {
-        Task<List<UserRoleResponseDto>> GetAll(int page, int pageSize);
+        Task<List<UserRoleResponseDto>> GetAll(int page = 1, int pageSize = 10);
         Task<UserRoleResponseDto?> Get(int id);
         Task<UserRoleResponseDto> Create(UserRoleCreateDto dto);
         Task<UserRoleResponseDto> Update(int id, UserRoleUpdateDto dto);
