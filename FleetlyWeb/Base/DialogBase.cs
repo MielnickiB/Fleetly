53df6e154e6a402db7dc9d5e99262a0f3183f309
MielnickiB <mielnicki.bartek@gmail.com>
2025-12-20 03:29:36 +0100

    Duża zmiana, dodanie DialogBase i LocationForm. Pomoże to w ujednoliceniu dialogów i zasadzie DRY. Dodałem również nowe DTO LocationBase, powinienem to zrobić w każdym modelu, który będzie edytowany i tworzony.

---
+++ FleetlyWeb/Base/DialogBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace FleetlyWeb.Base;
+
+public abstract class DialogBase<TModel> : ComponentBase where TModel : class, new()
+{
+    [CascadingParameter] protected IMudDialogInstance MudDialog { get; set; } = default!;
+    [Inject] protected ISnackbar Snackbar { get; set; } = default!;
+
+    protected TModel Model { get; set; } = new();
+    protected bool IsBusy = false;
+
+    protected EditForm? EditForm;
+    protected EditContext? EditContext;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender && EditForm?.EditContext != null)
+        {
+            EditContext = EditForm.EditContext;
+            StateHasChanged();
+        }
+    }
+
+    protected void Cancel() => MudDialog.Cancel();
+
+    protected async Task SubmitForm()
+    {
+        if (EditContext is null) return;
+
+        if (!EditContext.Validate())
+        {
+            Snackbar.Add("Popraw błędy w formularzu", Severity.Warning);
+            return;
+        }
+
+        IsBusy = true;
+        try
+        {
+            await Submit();
+        }
+        catch (Exception ex)
+        {
+            Snackbar.Add($"Wystąpił nieoczekiwany błąd: {ex.Message}", Severity.Error);
+        }
+        finally
+        {
+            IsBusy = false;
+            StateHasChanged();
+        }
+    }
+
+    protected abstract Task Submit();
+}
