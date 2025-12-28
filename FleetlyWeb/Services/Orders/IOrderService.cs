8c40fda7ecf7e6ccb8c2a8f1e760fd6d0a1d91a0
MielnickiB <mielnicki.bartek@gmail.com>
2025-12-29 00:01:17 +0100

    Poprawa działania serwisu zleceń na web-ówce.

---
+++ FleetlyWeb/Services/Orders/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using Fleetly.Shared.Dto;
+using Fleetly.Shared.Dto.OrderDtos;
+
+namespace FleetlyWeb.Services.Orders
+{
+    public interface IOrderService
+    {
+        Task<ApiResponse<PagedResult<OrderResponseDto>?>> GetAllOrdersAsync();
+        Task<ApiResponse<OrderResponseDto?>> GetOrderByIdAsync(int orderId);
+
+        Task<ApiResponse<OrderResponseDto?>> CreateOrderAsync(OrderCreateDto dto);
+        Task<ApiResponse<OrderResponseDto?>> UpdateOrderAsync(int orderId, OrderUpdateDto dto);
+
+        Task<ApiResponse<OrderResponseDto?>> CancelOrderAsync(int orderId);
+        Task<ApiResponse<OrderResponseDto?>> ApproveOrderAsync(int orderId);
+        Task<ApiResponse<OrderResponseDto?>> ApproveCostsAsync(int orderId);
+    }
+}
