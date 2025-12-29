0f7e27f5bd17ca92003bbec361ec56a51edb18bb
MielnickiB <mielnicki.bartek@gmail.com>
2025-12-30 00:26:40 +0100

    Dodałemprzykładowy formularz dodawania zlecenia (do weryfikacji i poprawy).

--- Fleetly.Shared/Dto/OrderDtos/OrderBaseDto.cs
+++ Fleetly.Shared/Dto/OrderDtos/OrderBaseDto.cs
@@ -16,15 +16,18 @@ namespace Fleetly.Shared.Dto.OrderDtos
         public int? ServiceLocationId { get; set; }
 
         [Required(ErrorMessage = "Dystans jest wymagany!")]
-        [Range(1, 5000, ErrorMessage = "Dystans musi być większy od 0!")]
+        [Range(1, 5000, ErrorMessage = "Dystans musi być większy od 0 i mniejszy od 5000!")]
         public int RangeOfKm { get; set; }
 
         [Required(ErrorMessage = "Data rozpoczęcia jest wymagana!")]
+        [DataType(DataType.DateTime, ErrorMessage = "Niepoprawny format daty i godziny!")]
         public DateTime StartTime { get; set; }
 
         [Required(ErrorMessage = "Data zakończenia jest wymagana!")]
+        [DataType(DataType.DateTime, ErrorMessage = "Niepoprawny format daty i godziny!")]
         public DateTime Deadline { get; set; }
 
+        [DataType(DataType.DateTime, ErrorMessage = "Niepoprawny format daty i godziny!")]
         public DateTime? ServiceTime { get; set; }
 
         public string? Details { get; set; }
