1332eacf3e7d37887844c25df3f07efff92adb55
MielnickiB <mielnicki.bartek@gmail.com>
2026-01-03 01:31:26 +0100

    Serwis notyfikacji i podstawowa wersja badge'a.

---
+++ FleetlyWeb/Services/Notifications/INotificationService.cs
@@ -0,0 +1,15 @@
+﻿using Fleetly.Shared.Dto.NotificationDtos;
+
+namespace FleetlyWeb.Services.Notifications
+{
+    public interface INotificationService
+    {
+        Task<ApiResponse<List<NotificationResponseDto>>> GetMyNotificationsAsync();
+        Task<ApiResponse<int>> GetUnreadCountAsync();
+        Task<ApiResponse<bool>> MarkAsReadAsync(int id);
+        Task<ApiResponse<bool>> MarkAllAsReadAsync();
+        Task<ApiResponse<bool>> DeleteAsync(int id);
+
+        event Action? OnChange;
+    }
+}
