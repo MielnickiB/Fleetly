2303bdb07b3e011cbba9374d82aec5228b1537ca
MielnickiB <mielnicki.bartek@gmail.com>
2026-01-16 01:00:19 +0100

    Poprawienie minimalizacji aplikacji. Od teraz kiedy ją zminimalizujemy powróci do miejsca w którym byliśmy.

--- FleetlyMobile/Platforms/Android/MainActivity.cs
+++ FleetlyMobile/Platforms/Android/MainActivity.cs
@@ -3,7 +3,7 @@ using Android.Content.PM;
 
 namespace FleetlyMobile
 {
-    [Activity(Theme = "@style/FleetlyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/FleetlyTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
